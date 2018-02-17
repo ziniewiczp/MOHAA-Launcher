2845146f652b09adaeccc985cc8fbc10b24139ff
ziniewiczp <ziniewicz.p@gmail.com>
2018-02-17 15:02:38 +0100

    add NotificationManager class

---
+++ src/NotificationManager.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class NotificationManager {
+
+    static void displayNotification(String message) {
+        JOptionPane.showMessageDialog(new JFrame(), message);
+    }
+
+}
